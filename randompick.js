e6da8d6cc2f461c3f22eb26fdefff293f622c7c3
SlavaLysjuk <vlysjuk@gmail.com>
2020-08-23 14:59:23 +0300

    feat: randompic

---
+++ randompick.js
@@ -0,0 +1,29 @@
+const {webkit, chromium, firefox, devices} = require('playwright');
+
+(async () => {
+		const browser = await chromium.launch({
+			headless: false,
+		});
+		const context = await browser.newContext({
+		});
+		const page = await context.newPage();
+		
+		context.route('**/*', route => {
+			if (route.request().resourceType() !== 'image')
+				return route.continue();
+
+			const LOREM_FLICKR = 'https://loremflickr.com';
+			if (route.request().url().startsWith(LOREM_FLICKR))
+				return route.continue();
+
+			route.fulfill({
+				status: 301,
+				headers: {
+					location: LOREM_FLICKR + '/320/240/parot',
+				}
+			})
+		});
+		await page.goto('https://theverge.com');
+})();
+
+
