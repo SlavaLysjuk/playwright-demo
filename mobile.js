48be3e5a68bc4d675e9923a8ec95e304f27c3db8
SlavaLysjuk <vlysjuk@gmail.com>
2020-08-23 14:18:50 +0300

    feat: more stuff!

--- mobile.js
+++ mobile.js
@@ -1,15 +1,24 @@
 const {webkit, chromium, firefox, devices} = require('playwright');
-const iPhone11Pro = devices['iPhone 11 Pro'];
 
 (async () => {
-		const browser = await webkit.launch({
+		const browser = await chromium.launch({
 			headless: false,
 		});
 		const context = await browser.newContext({
-			...iPhone11Pro,
+			geolocation: {
+				latitude: 51.5080703,
+				longitude: -0.0993819,
+			},
+			permissions: ['geolocation'],
+			locale: 'de-DE',
+			colorScheme: 'dark',			
 		});
 		const page = await context.newPage();
-		await page.goto('https://github.com/microsoft/playwright');
+		await page.goto('https://overreacted.io');
+		await page.waitForTimeout(1000);
+		await page.emulateMedia({
+			colorScheme: 'light',
+		})
 })();
 
 
